7e36808c0b778a774f4c88603ee8262b34b687d7
jairo-sousa <jairo.sousa.rufino@gmail.com>
2025-01-19 19:07:04 -0300

    feat: use chakra instead of css

--- src/PunchInKioske.tsx
+++ src/PunchInKioske.tsx
@@ -1,16 +1,90 @@
-import "./PunchInKioske.css";
+import { Flex, Input, Text } from "@chakra-ui/react";
+import { Field } from "@/components/ui/field";
+
+import { Button, Fieldset } from "@chakra-ui/react";
 
 export function PunchInKioske() {
     return (
-        <div className="LoginContainer">
-            <h1 className="Logo">REGISTRO DE PONTO</h1>
-            <p>Digite seu cpf para efetuar um registro</p>
+        <Flex
+            // BRAND GRADIENT PANEL
+            w={"100%"}
+            h={"100%"}
+            justify={"center"}
+            align={"center"}
+            background={
+                "linear-gradient(71.17deg, #feaf00 19.35%, #f8d442 90.12%)"
+            }
+        >
+            <Flex
+                // PRIMARY MODAL PANEL
+                direction={"column"}
+                justify={"center"}
+                align={"center"}
+                w={"47.5rem"}
+                h={"fit-content"}
+                gap={"3rem"}
+                p={"4.4rem 3rem"}
+                borderRadius={"2rem"}
+                backgroundColor={"var(--background-primary)"}
+                boxShadow={"2px 5px 10px rgba(0, 0, 0, 0.1)"}
+            >
+                <Text
+                    // LOGO
+                    as={"h1"}
+                    pl={"1.2rem"}
+                    borderLeft={"0.6rem solid #f8d442"}
+                >
+                    REGISTRO DE PONTO
+                </Text>
 
-            <fieldset>
-                <label htmlFor="cpf">CPF</label>
-                <input id="cpf" type="text" name="cpf" />
-            </fieldset>
-            <button>REGISTRAR</button>
-        </div>
+                <Text
+                    // SUBTITLE
+                    color={"var(--content-secondary)"}
+                    fontSize={"1.4rem"}
+                    fontWeight={400}
+                >
+                    Digite seu cpf para efetuar um registro
+                </Text>
+
+                <Fieldset.Root w={"100%"} h={"fit-content"} gap={"3rem"}>
+                    <Fieldset.Content>
+                        <Field
+                            label="CPF"
+                            fontSize={"1.4rem"}
+                            fontWeight={400}
+                            color={"var(--content-primary)"}
+                            gap={"1rem"}
+                        >
+                            <Input
+                                borderRadius={"0.4rem"}
+                                p={"2rem"}
+                                fontSize={"1.4rem"}
+                                fontWeight={400}
+                                outlineWidth={0}
+                                border={"1px solid var(--border-secondary)"}
+                                color={"var(--content-secondary)"}
+                                placeholder="Digite seu cpf"
+                                _placeholder={{
+                                    color: "#CDCDCD",
+                                }}
+                            />
+                        </Field>
+                    </Fieldset.Content>
+
+                    <Button
+                        // FORM BUTTON
+                        type="submit"
+                        borderRadius={"0.4rem"}
+                        p={"2rem"}
+                        m={0}
+                        fontSize={"1.4rem"}
+                        fontWeight={400}
+                        backgroundColor={"var(--content-brand)"}
+                    >
+                        REGISTRAR
+                    </Button>
+                </Fieldset.Root>
+            </Flex>
+        </Flex>
     );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+
+import { ChakraProvider } from "@chakra-ui/react";
+import system from "./theme/theme";
+
 import { PunchInKioske } from "./PunchInKioske";
-import "./index.css";
 
 document.addEventListener("DOMContentLoaded", function () {
     const container = document.getElementById("root")!;
     const root = createRoot(container);
     root.render(
         <StrictMode>
-            <PunchInKioske />
+            <ChakraProvider value={system}>
+                <PunchInKioske />
+            </ChakraProvider>
         </StrictMode>
     );
 });
