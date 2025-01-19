de2fa38cf220d8f4b537613e5b4359958a7c9839
jairo-sousa <jairo.sousa.rufino@gmail.com>
2025-01-19 19:04:59 -0300

    build: add chakra-ui

---
+++ src/theme/theme.ts
@@ -0,0 +1,77 @@
+import { system } from "@chakra-ui/react/preset";
+
+const newStyles = {
+    ":root": {
+        fontSize: "62.5%",
+
+        textRendering: "optimizeLegibility",
+        WebkitFontSmoothing: "antialiased",
+        MozOsxFontSmoothing: "grayscale",
+
+        // CONTENT
+        "--content-primary": "#000",
+        "--content-secondary": "#6C6C6C",
+        "--content-tertiary": "#CDCDCD",
+        "--content-brand": "#FEAF00",
+        "--content-brand-contrast": "var(--background-primary)",
+        "--content-success": "#40C026",
+        "--content-error": "#FF0000",
+
+        // BACKGROUND
+        "--background-primary": "#fff",
+        "--background-secondary": "#F8F8F8",
+        "--background-tertiary": "#F2EAE1",
+        "--background-brand": "var(--content-brand)",
+
+        // BORDER
+        "--border-primary": "var(--content-brand)",
+        "--border-secondary": "#E5E5E5",
+    },
+
+    "*": {
+        margin: 0,
+        padding: 0,
+        boxSizing: "border-box",
+    },
+
+    body: {
+        width: "100%",
+        height: "100vh",
+        backgroundColor: "var(--background-primary)",
+        color: "var(--content-primary)",
+
+        fontFamily: "Montserrat",
+        fontSize: "1.6rem",
+        fontWeight: 500,
+    },
+
+    "#root": {
+        height: "100%",
+        width: "100%",
+
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+
+    a: {
+        textDecoration: "inherit",
+    },
+
+    "::placeholder": {
+        color: "var(--content-tertiary)",
+        opacity: 1,
+    },
+
+    "::-ms-input-placeholder": {
+        color: "var(--content-tertiary)",
+        opacity: 1,
+    },
+    "input::placeholder": {
+        color: "red",
+    },
+};
+
+Object.assign(system._global[0]["@layer reset"]!, newStyles);
+
+export default system;
