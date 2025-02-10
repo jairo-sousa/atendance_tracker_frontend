dd0064c8a1ab41a39bd45bea8de6499a5d0c2b2f
jairo-sousa <jairo.sousa.rufino@gmail.com>
2025-02-09 23:37:17 -0300

    bugfix(sidebar): fix rendering issue and prevent flashing

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -7,15 +7,17 @@ import { LinksContainer } from "@/fragments/sidebar/LinksContainer";
 import { LogoutButton } from "@/fragments/sidebar/LogoutButton";
 import { SidebarLinkList } from "@/fragments/sidebar/SidebarLinkList";
 import { Toaster } from "./ui/toaster";
+import { useLocation } from "react-router";
 
 export function SideBar() {
-    const [hasSessionToken, setHasSessionToken] = useState<boolean>();
+    const [hasSessionToken, setHasSessionToken] = useState<boolean>(false);
+    const router = useLocation();
 
     useEffect(() => {
         setHasSessionToken(
             Cookies.get("sessionToken")?.startsWith("Bearer ") == true
         );
-    }, []);
+    }, [router]);
 
     if (hasSessionToken == false) return;
 
