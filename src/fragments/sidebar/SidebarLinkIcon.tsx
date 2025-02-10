9b6211742d5996bb33890dd1a45b043b9b9879ce
jairo-sousa <jairo.sousa.rufino@gmail.com>
2025-02-09 23:20:09 -0300

    hotfix: use root img path

--- src/fragments/sidebar/SidebarLinkIcon.tsx
+++ src/fragments/sidebar/SidebarLinkIcon.tsx
@@ -5,10 +5,6 @@ interface SidebarLinkIconInterface {
 }
 export function SidebarLinkIcon({ imagePath }: SidebarLinkIconInterface) {
     return (
-        <Image
-            src={`/public${imagePath}.svg`}
-            objectFit={"contain"}
-            h={"1.7rem"}
-        />
+        <Image src={`${imagePath}.svg`} objectFit={"contain"} h={"1.7rem"} />
     );
 }
