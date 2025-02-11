b5178c08180173df93c28c238d247019fc3a1614
jairo-sousa <jairo.sousa.rufino@gmail.com>
2025-02-11 15:02:40 -0300

    refactor: add SecondaryDivisor vertical option

--- src/fragments/layout/SecondaryDivisor.tsx
+++ src/fragments/layout/SecondaryDivisor.tsx
@@ -1,10 +1,18 @@
+import { globalColors } from "@/theme/theme";
 import { Box } from "@chakra-ui/react";
 
-export function SecondaryDivisor() {
+interface SecondaryDivisorInterface {
+    vertical?: boolean;
+}
+
+export function SecondaryDivisor({ vertical }: SecondaryDivisorInterface) {
+    const { borderSecondary } = globalColors;
+
     return (
         <Box
-            w={"100%"}
-            borderBottom={"1px solid var(--border-secondary)"}
+            w={vertical ? "1px" : "100%"}
+            h={vertical ? "100%" : "1px"}
+            backgroundColor={borderSecondary}
         ></Box>
     );
 }
